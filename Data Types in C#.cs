26721eefb3ac109ea41f9f2fe57944528fdbfbb2
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-11-23 00:53:19 +0000

    Add files via upload

---
+++ Data Types in C#.cs
@@ -0,0 +1,17 @@
+/*
+ 
+
+ Data Types in C#
+
+1. int: Stands for integer.
+2. byte: Value type that represents unsigned integers with values that range from 0 to 255.
+3. float: Fefers to floating point numbers as 12.43, 5.2 and -9.12.
+4. double: Represents a double-precision floating-point number.
+5. decimal: Stores a decimal number but has a smaller range than float 28-29.
+6. char: Stands for character and is used to store single Unicode characters.
+7. bool: Stands for boolean and can only hold two values: true and false.
+ 
+ ByDenisRafi
+
+ */
+
